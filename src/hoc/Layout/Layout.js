8c89cfdfc1e84861708396c20b76fcd22fd017ac
Sofia Kalinichenko <kalinichenko@pixelfield.cz>
2019-12-17 17:13:00 +0100

    menu list and backdrop

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from './Layout.css'
 import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
+import Drawer from '../../components/Navigation/Drawer/Drawer'
 
 class Layout extends Component {
 
@@ -14,9 +15,21 @@ class Layout extends Component {
         })
     }
 
+    menuCloseHandler = () => {
+        this.setState({
+            menu: false
+        })
+    }
+
     render() {
         return(
             <div className={classes.Layout}>
+
+                <Drawer 
+                    isOpen={this.state.menu}
+                    onClose={this.menuCloseHandler}
+                />
+
                 <MenuToggle 
                     onToggle={this.toggleMenuHandler}
                     isOpen={this.state.menu}
